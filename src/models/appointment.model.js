610164e3130bc01b9bbed7567e43448dcbc1a516
Recurso Am <kicristian250@gmail.com>
2026-03-01 20:06:25 -0500

    Se agrego la actualizacion y eliminacion de tareas

--- src/models/appointment.model.js
+++ src/models/appointment.model.js
@@ -1,16 +1,17 @@
 const db = require('./db');
 const { v4: uuidv4 } = require('uuid');
 
-function hasOverlap(start, end, callback) {
+function hasOverlap(start, end, excludeId, callback) {
   const query = `
     SELECT COUNT(*) as count
     FROM appointments
     WHERE
       start_time < ?
       AND end_time > ?
+      AND id != ?
   `;
 
-  db.get(query, [end, start], (err, row) => {
+  db.get(query, [end, start, excludeId], (err, row) => {
     if (err) callback(err);
     else callback(null, row.count > 0);
   });
@@ -18,50 +19,79 @@ function hasOverlap(start, end, callback) {
 
 function createAppointment(data, callback) {
   const { title, start_time, end_time, description } = data;
-
   const id = uuidv4();
 
-  hasOverlap(start_time, end_time, (err, overlap) => {
+  hasOverlap(start_time, end_time, '', (err, overlap) => {
     if (err) return callback(err);
-
-    if (overlap) {
-      return callback(new Error('OVERLAP'));
-    }
+    if (overlap) return callback(new Error('OVERLAP'));
 
     const query = `
       INSERT INTO appointments (id, title, start_time, end_time, description)
       VALUES (?, ?, ?, ?, ?)
     `;
 
-    db.run(
-      query,
-      [id, title, start_time, end_time, description],
-      function (err) {
-        if (err) callback(err);
-        else callback(null, { id, title, start_time, end_time, description });
-      }
-    );
+    db.run(query, [id, title, start_time, end_time, description], err => {
+      if (err) callback(err);
+      else callback(null, { id, title, start_time, end_time, description });
+    });
   });
 }
 
-function getAllAppointments(callback) {
-  db.all(
-    `SELECT * FROM appointments ORDER BY start_time ASC`,
-    [],
-    callback
+function updateAppointment(id, data, callback) {
+  const { title, start_time, end_time, description } = data;
+
+  getAppointmentById(id, (err, existing) => {
+    if (err) return callback(err);
+    if (!existing) return callback(new Error('NOT_FOUND'));
+
+    hasOverlap(start_time, end_time, id, (err, overlap) => {
+      if (err) return callback(err);
+      if (overlap) return callback(new Error('OVERLAP'));
+
+      const query = `
+        UPDATE appointments
+        SET title = ?, start_time = ?, end_time = ?, description = ?
+        WHERE id = ?
+      `;
+
+      db.run(
+        query,
+        [title, start_time, end_time, description, id],
+        function (err) {
+          if (err) callback(err);
+          else callback(null, { id, title, start_time, end_time, description });
+        }
+      );
+    });
+  });
+}
+
+function deleteAppointment(id, callback) {
+  db.run(
+    `DELETE FROM appointments WHERE id = ?`,
+    [id],
+    function (err) {
+      if (err) return callback(err);
+      if (this.changes === 0) {
+        return callback(new Error('NOT_FOUND'));
+      }
+      callback(null, true);
+    }
   );
 }
 
+function getAllAppointments(callback) {
+  db.all(`SELECT * FROM appointments ORDER BY start_time ASC`, [], callback);
+}
+
 function getAppointmentById(id, callback) {
-  db.get(
-    `SELECT * FROM appointments WHERE id = ?`,
-    [id],
-    callback
-  );
+  db.get(`SELECT * FROM appointments WHERE id = ?`, [id], callback);
 }
 
 module.exports = {
   createAppointment,
   getAllAppointments,
-  getAppointmentById
+  getAppointmentById,
+  updateAppointment,
+  deleteAppointment
 };
